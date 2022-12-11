c2b42154399d90282b32477754c513e5c6d4ed40
Apoorva Batra <120050628+apoorvabatra1@users.noreply.github.com>
2022-12-12 01:29:57 +0530

    Create add_2_integers.java
    [Task 2] Program to add two integers.

---
+++ add_2_integers.java
@@ -0,0 +1,12 @@
+class Main {
+
+  public static void main(String[] args) {
+    
+    int first = 70;
+    int second = 30;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+  }
+}
